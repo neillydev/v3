9ed30616f926da1b08f803b52937d3d78f7c281c
neillydev <neillydev@gmail.com>
2023-06-23 04:34:07 -0700

    add sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,49 @@
-import Module from '@/components/Module/Module'
+import Module from "@/components/Module/Module";
+import Section from "@/components/Section/Section";
+import Technologies from "@/components/Technologies/Technologies";
+import Experience from "@/components/Experience/Experience";
+import Projects from "@/components/Projects/Projects";
 
-import styles from './page.module.css';
+import styles from "./page.module.css";
 
 export default function Home() {
-  
   return (
-    <main className={styles.main}>
+    <main className={`${styles.main} font-mono`}>
       <Module />
+      <div className="relative mx-auto flex w-full max-w-4xl flex-col gap-16 overflow-hidden p-8 leading-relaxed text-blue-100 max-md:mt-16 md:p-16">
+        <Section 
+          id="section_about" 
+          title="Who Am I?" 
+          body="I'm Vernon Neilly, and I simply love programming. Since I were young, I've been captivated by the internet and computers, always eager to dig deeper into how it all works behind the scenes.
+          \n
+          Starting from scratch, I taught myself how to build websites using HTML, Javascript, and CSS. This led me down a path where I started building plugins and web applications, first as a freelancer for a wide range of clients, and later as a part of larger teams in multiple corporate settings.
+          \n
+          The knowledge and experience that I've gathered over the years across these various avenues have equipped me with a proficiency in delivering effective solutions.
+          \n
+          Below are technologies I work with consistently:" 
+          module={<Technologies />}
+        />
+        <br />
+        <Section 
+          id="section_exp" 
+          title="Where I've Been" 
+          body="" 
+          module={<Experience />}
+        />
+        <br />
+        <Section 
+          id="section_exp" 
+          title="What I've Created" 
+          body="" 
+          module={<Projects />}
+        />
+        <br />
+        <Section 
+          id="section_exp" 
+          title="Contact" 
+          body="" 
+        />
+      </div>
     </main>
-  )
+  );
 }
