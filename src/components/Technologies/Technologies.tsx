d4686c4e669cb67dbe151bf5e44f48c5353da690
neillydev <neillydev@gmail.com>
2023-06-24 08:45:24 -0700

    create technologies module

---
+++ src/components/Technologies/Technologies.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Technologies = () => {
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      <p>• ReactJS</p>
+      <p>• NextJS</p>
+      <p>• Vue.js</p>
+      <p>• Golang</p>
+      <p>• Python</p>
+      <p>• Javascript/Typescript</p>
+      <p>• GraphQL</p>
+    </div>
+  )
+}
+
+export default Technologies
