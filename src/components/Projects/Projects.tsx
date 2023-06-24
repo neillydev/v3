f5a0b15615be885ee26697fbbf9f0be94e6cd53a
neillydev <neillydev@gmail.com>
2023-06-24 13:10:24 -0700

    fix bug

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -69,6 +69,7 @@ const Projects = () => {
           {project.technologies.map(
             (technology: string, index: number) => (
               <p
+                key={index}
                 className={
                   index === project.technologies.length - 1
                     ? ""
