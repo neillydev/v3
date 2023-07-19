513211c030e0481005454ae183e5320c4156ad43
neillycubed <neillybusiness3@gmail.com>
2023-07-19 02:30:34 -0700

    refactor experience achievements

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -14,8 +14,8 @@ const Experience = () => {
       duration: "Nov. 2022 - Present",
       achievements: [
         "Pioneered the adoption and integration of machine learning models for automating media content, enabling the automatic generation of videos, resulting in a remarkable 35% uptick in publisher video production, improving publisher engagement.",
-        "Devised and implemented an algorithm capable of identifying and calculating trending search terms amongst publishers across diverse intervals, enhancing the company's ability to respond quickly to dynamic media-search trends.",
-        "Refactored critical Golang backend API modules alongside the development of various front-end Vue.js components, significantly improving user experience and code maintainability, as well as ensuring seamless integration with existing and future systems.",
+        "Devised and implemented an algorithm capable of identifying and calculating trending search terms amongst publishers, enhancing my team's ability to respond quickly to media-search trends.",
+        "Refactored critical Golang API modules alongside the development of various Vue.js components, significantly improving user experience and code maintainability.",
         "Implemented serverless functions with an API using OpenFAAS, integrating machine learning libraries with a robust Golang backend.",
       ],
       company: true,
