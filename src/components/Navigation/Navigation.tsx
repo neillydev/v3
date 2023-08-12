7e1e625674c790510fde196d8fd15d01cbd7db96
neillycubed <neillybusiness3@gmail.com>
2023-08-11 19:30:26 -0700

    fix styling

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -5,7 +5,7 @@ import LinkedInSVG from "@/../public/linkedin.svg";
 
 const Navigation = () => {
   return (
-    <div className="flex flex-row space-between absolute inset-x-14 bottom-0 mt-8 mb-8 mr-12 ml-12 justify-center">
+    <div className="flex flex-row space-between absolute inset-x-14 bottom-0 mt-8 mr-12 ml-12 justify-center">
       <div className="flex flex-1 flex-col gap-4 md:flex-row md:gap-0">
         <a href="#about">
           <button className="mx-2 flex gap-2 justify-center items-center w-fit font-medium basis-1 gap-2 justify-center items-center p-2 px-5 min-w-max text-sm font-bold text-center hover:.!text-[#09073a] hover:.bg-white hover:bg-opacity-100 active:bg-opacity-90 hover:.outline-white .outline  rounded-full hover:.outline-offset-4 active:.outline-offset-2 backdrop-blur-sm transition-all md:text-base outline-[#09073a]/50 bg-violet-500/20 text-rose-50 hover:bg-violet-500">
